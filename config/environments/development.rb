874edcf293e255b8bd7490945ea2be7a76098c51
EC2 Default User <ec2-user@ip-172-31-17-126.ap-northeast-1.compute.internal>
2022-10-03 11:58:33 +0000

    [add]deviseのinstall

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "514ad6d376464b5ba586480937017d2a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
