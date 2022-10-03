dc20259815ac9a6ef3bc210a23b06989e2cc3df4
kazukihoshi <starrysky.blue30@gmail.com>
2022-10-03 11:58:33 +0000

    [add]deviseのinstall

--- db/migrate/20221003111348_create_posts.rb
+++ db/migrate/20221003111348_create_posts.rb
@@ -1,7 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      
+      t.integer :user_id, null: false
+      t.text :post, null: false
+      t.string :camp_site, null: false
+      t.string :area, null: false
+      t.string :prefecture, null: false
+      t.string :location, null: false
+      t.string :scene, null: false
+      t.string :facility_type, null: 
       t.timestamps
     end
   end
