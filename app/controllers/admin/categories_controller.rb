6e77bd51357ba10ba9c6d39c781b08515bc73830
kazukihoshi <starrysky.blue30@gmail.com>
2022-10-06 11:55:08 +0000

    [add]bootatrapエラー修正完了

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,25 @@
 class Admin::CategoriesController < ApplicationController
   def index
     @category = Category.new
+    @categories = Category.all
   end
 
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to admin_categories_path
+  end
+  
+  def create
+    category = Category.new(category_params)
+    category.save
+    redirect_to admin_categories_path
+  end
+
+  private
+  
+  def category_params
+    params.require(:category).permit(:category_name)
+  end
 
 end
