a369d48d8e0f436fd3ca1d009fd79e49a1924429
EC2 Default User <ec2-user@ip-172-31-17-126.ap-northeast-1.compute.internal>
2022-10-07 12:51:32 +0000

    [add]categoryのdestroyのエラー

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -15,6 +15,12 @@ class Admin::CategoriesController < ApplicationController
     category.save
     redirect_to admin_categories_path
   end
+  
+  def update
+    category = Category.find(params[:id])
+    category.update(category_params)
+    redirect_to admin_categories_path
+  end
 
   private
   
