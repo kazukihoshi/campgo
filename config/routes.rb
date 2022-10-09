f94e75abafcaa92257bcf82721d584b5a14e6411
kazukihoshi <starrysky.blue30@gmail.com>
2022-10-09 11:20:53 +0000

    [add]scheduleの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   scope module: :public do
     root to: 'homes#top'
     resources :users, only: [:index, :edit, :update]
@@ -11,14 +11,14 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :edit, :update, :destroy]
     resources :tags, only: [:create, :destroy]
     resources :favorites, only: [:index, :create, :destroy]
-    resources :camps, only: [:new, :show, :edit, :update, :destroy]
+    resources :camps, only: [:new, :create, :show, :edit, :update, :destroy]
     resources :checklists, only: [:edit]
     resources :checklist_manages, only: [:update]
   end
-  
+
   namespace :admin do
-    
-   
+
+
     resources :users, only: [:index, :show, :edit, :update]
     resources :posts, only: [:index, :show, :edti, :update, :destroy]
     resources :categories, only: [:index, :create, :edit, :update] do
@@ -30,15 +30,15 @@ Rails.application.routes.draw do
     get 'camps/search' => 'camps#search'
     get '/' => 'homes#top'
   end
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
