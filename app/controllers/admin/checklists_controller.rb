9387999871fb108a5ea222945b6c539837a0e4d3
kazukihoshi <starrysky.blue30@gmail.com>
2022-10-10 21:15:03 +0000

    [add]admin/checklist/indexエラー

--- app/controllers/admin/checklists_controller.rb
+++ app/controllers/admin/checklists_controller.rb
@@ -1,7 +1,6 @@
 class Admin::ChecklistsController < ApplicationController
   def index
     @checklists = Checklist.all
-   
     @category = Category.find(params[:category_id])
   end
 
@@ -13,26 +12,26 @@ class Admin::ChecklistsController < ApplicationController
     #byebug
     redirect_to admin_category_checklists_path(@category)
   end
-  
+
   def show
   end
 
   def edit
   end
-  
+
   def destroy_all
     checked_date = params[:deletes].keys
     if Checklist.destroy_by(checked_date)
       redirect_to admin_category_checklists_path(@category)
     else
       render :index
-    end 
+    end
   end
-  
+
   private
-  
+
   def checklist_params
-    params.require(:checklist).permit(:checklist_name, :category_id, :comment)
+    params.require(:checklist).permit(:checklist_name, :comment, :category_id)
   end
 
 end
