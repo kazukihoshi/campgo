3d9461758879ccdb6705a0475d12fb525c841dd8
EC2 Default User <ec2-user@ip-172-31-17-126.ap-northeast-1.compute.internal>
2022-10-11 13:42:48 +0000

    [add]checklist/index_controller編集

--- app/controllers/public/checklists_controller.rb
+++ app/controllers/public/checklists_controller.rb
@@ -1,4 +1,17 @@
 class Public::ChecklistsController < ApplicationController
   def edit
   end
+
+  def index
+    @camp = current_user.camps.new(camp_params)
+    @checklist = Checklist.find(params[:id])
+  end
+
+
+  private
+
+  def checklist_params
+    params.require(:checklist).permit(:checklist_name, :comment)
+  end
+
 end
