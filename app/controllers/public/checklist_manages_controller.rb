9d9e84f5f7c3681facbd5f43b376d55c5cc304b0
kazukihoshi <starrysky.blue30@gmail.com>
2022-10-23 13:02:12 +0000

    [add]checklist_manage/update_camp_checklist_manageの作成

--- app/controllers/public/checklist_manages_controller.rb
+++ app/controllers/public/checklist_manages_controller.rb
@@ -2,8 +2,11 @@ class Public::ChecklistManagesController < ApplicationController
 
 
   def update_camp_checklist_manage
-    
-    old_checklist_manages = 
+    #tag_list = tags.split(/[[:blank:]]+/)
+    active_checklist_ids = @camp.checklist_manages.where(is_active: true).pluck('checklist_id').uniq #[2,3,6,9]
+    @checklists = Checklist.where(id: active_checklist_ids)
+    checklist_list = checklists.split(/[[:blank:]]+/)
+    new_checklist_manages = checklist_list - current_user.checklist.id
   
   end
 
