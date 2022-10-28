8cba6276fa560aca4e7e47ce777773dde3673915
EC2 Default User <starrysky.blue30@gmail.com>
2022-10-28 23:44:41 +0000

    [add]post/newの作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,7 +6,7 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    tag_list = params[:post][:name].split(',')
+    tag_list = params[:post][:name]
     if @post.save
       @post.save_tag(tag_list)
       redirect_to posts_path
@@ -25,4 +25,12 @@ class Public::PostsController < ApplicationController
 
   def edit
   end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post, :camp_site, :name)
+  end
+
 end
