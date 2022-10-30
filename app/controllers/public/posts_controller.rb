42ab9201ecab24c8bb71759447f351a4f4ee1365
EC2 Default User <starrysky.blue30@gmail.com>
2022-10-30 09:16:24 +0000

    [add]tagのエラー

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,9 +6,9 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    tag_list = params[:post][:name]
+    tag_list = params[:post][:tag]
     if @post.save
-      @post.save_tag(tag_list)
+      @post.save_tags(tag_list)
       redirect_to posts_path
     else
       render :new
@@ -30,7 +30,7 @@ class Public::PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:post, :camp_site, :tag.name)
+    params.require(:post).permit(:post, :title, :camp_site, :tag.name)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,29 @@
 class Post < ApplicationRecord
-    
-    has_many :comments, dependent: :destroy
+
+    has_many :post_tags, dependent: :destroy#中間テーブルとのアソシエーションを先に記載
     has_many :tags, through: :post_tags
+
     has_many :favorites, dependent: :destroy
-    
+    has_many :comments, dependent: :destroy
+
     has_many_attached :post_image
-    
+
+
+    def save_tags(tags)
+
+        tag_list = tags.split(/[[:blank:]]+/)
+        current_tags =self.tags.pluck(:name)
+        old_tags = current_tags - tag_list
+        new_tags = tag_list - current_tags
+
+        old_tags.each do |old|
+          self.tags.delete Tag.find_by(name: old)
+        end
+
+        new_tags.each do |new|
+            new_post_tag = Tag.find_or_create_by(name: new)
+            self.tags << new_post_tag
+        end
+
+    end
 end
