980587845cf6e06cf1d8ccdd89964c9671630d0d
EC2 Default User <starrysky.blue30@gmail.com>
2022-10-31 05:29:06 +0000

    [add]post/indexの表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
     has_many :favorites, dependent: :destroy
     has_many :comments, dependent: :destroy
+    belongs_to :user
 
     has_many_attached :post_images
 
