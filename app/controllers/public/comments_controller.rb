6a2b5d6b1dd4f92f0aac7a8f7e41b5d942b95c5c
EC2 Default User <starrysky.blue30@gmail.com>
2022-10-31 20:29:51 +0000

    [add]postの削除

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -15,6 +15,11 @@ class Public::CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment.find_by(id :params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_path(post.id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment)
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -31,6 +31,12 @@ class Public::PostsController < ApplicationController
 
   def edit
   end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
 
 
   private
