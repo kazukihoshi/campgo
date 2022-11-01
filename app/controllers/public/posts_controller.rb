0ed29ec1704e2df3154eeb0f484cc908531bc5bf
EC2 Default User <starrysky.blue30@gmail.com>
2022-11-01 13:49:26 +0000

    [add]post/update

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -35,8 +35,13 @@ class Public::PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    tag_list = params[:tag]
+    if post.update(post_params)
+      post.save_tags(tag_list)
+      redirect_to post_path(post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
