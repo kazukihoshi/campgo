5a3fbad78ffc1e3ee1e3e8791d6508b4dd8cd3e4
EC2 Default User <starrysky.blue30@gmail.com>
2022-11-11 21:39:31 +0000

    [add]deviseの日本語化

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,9 +11,15 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to admin_user_path(user)
+  end
+
   private
   def user_params
-    params.require(:user).permit()
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
