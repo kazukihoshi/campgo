d736b8b220a697543e73be9489b2d1497c172ec7
EC2 Default User <starrysky.blue30@gmail.com>
2022-12-01 12:18:00 +0000

    [add]user/adminの権限の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,36 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!, except: [:top]
+  before_action :authenticate, except: [:top]
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
 
 
+  def authenticate
+    if admin_signed_in?
+      return true
+    elsif user_signed_in?
+      if request.fullpath.include?("/admin") &&
+        request.fullpath.include?(new_admin_session_path) == false # 二重リダイレクト防止
+        redirect_to new_admin_session_path
+        return false
+      end
+      return true
+    else
+      if request.fullpath.include?(new_user_session_path) == false && # 二重リダイレクト防止
+        request.fullpath.include?(new_user_registration_path) == false && # 二重リダイレクト防止
+        request.fullpath.include?(users_guest_sign_in_path) == false && # 二重リダイレクト防止
+        request.fullpath.include?(new_admin_session_path) == false # 二重リダイレクト防止
+        redirect_to new_user_session_path
+        return false
+      end
+    end
+  end
 
 end
